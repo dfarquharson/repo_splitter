a947c33847cd80a8b8be3d572fc480d81e32cabf
David Farquharson <djfarquharson@spscommerce.com>
2014-05-19 17:26:15 -0500

    using a generator for grabbing the next revision, which is kinda cool

--- finance.py
+++ finance.py
@@ -16,18 +16,23 @@ def get_log(repo):
 def get_next_revision(log):
     for l in log:
         if 'jstauffe' not in l['Author']:
-            return l, log.index(l)
+            yield l
 
 
 def get_repos(directory):
     entries = []
     for d in os.listdir(directory):
         log = get_log(directory+'/'+d)
-        last_rev, i = get_next_revision(log)
-        #next_rev = get_next_revision(log[i+1:])[0]
+        rev_generator = get_next_revision(log)
+        try:
+            last_rev = rev_generator.next()
+            prev_rev = rev_generator.next()
+        except StopIteration:
+            last_rev = ''
+            prev_rev = ''
         entries.append({'xnet': d,
                         'last_revision': last_rev,
-                        'previous_revision': '',
+                        'previous_revision': prev_rev,
                         'days_between_revisions': ''})
     return entries
 
