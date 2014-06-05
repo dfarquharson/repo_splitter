8e99691bc767be650186f8eadc150fa72fa9d876
David Farquharson <djfarquharson@spscommerce.com>
2014-06-05 15:08:39 +0000

    added ability to make repos remotely cloneable

---
+++ make_cloneable.py
@@ -0,0 +1,27 @@
+import os
+
+
+def append_config(d):
+    conf = d+'/.git/config'
+    if os.path.exists(conf):
+        with open(conf, 'a') as f:
+            f.write('[receive]\n\tdenyCurrentBranch = warn')
+
+
+def add_post_receive(d):
+    pr = d+'/.git/hooks/post-receive'
+    with open(pr, 'w') as f:
+        f.write('#!/bin/sh\n' +
+                'git --git-dir=. --work-tree=$PWD/.. reset --hard')
+    os.chmod(pr, 0775)
+
+
+def main():
+    for d in os.listdir('.'):
+        print(os.getcwd()+'/'+d)
+        append_config(d)
+        add_post_receive(d)
+
+
+if __name__ == '__main__':
+    main()
