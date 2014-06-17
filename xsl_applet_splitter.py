dc6c9ea96d474758f9710d2060bac1aabd12dfa5
David Farquharson <djfarquharson@spscommerce.com>
2014-06-17 14:45:12 -0500

    simple helper functions to filter the big git hairball

---
+++ xsl_applet_splitter.py
@@ -0,0 +1,27 @@
+import os
+import sys
+
+
+def list_files(cwd):
+    return [fmt_dir(parent, f)
+            for (parent, dirs, files) in os.walk(cwd)
+            for f in files if '.git' not in parent]
+
+
+def fmt_dir(path, f):
+    return str((path if path.endswith('/') else path+'/')+f)
+
+
+def get_files_with_extensions(cwd, exts=['.xsl', '.jpg', '.gif', '.png']):
+    return [fmt_dir(parent, f)
+            for (parent, dirs, files) in os.walk(cwd)
+            for f in files if '.git' not in parent and
+            any(f.endswith(ext) for ext in exts)]
+
+
+def delete_non_matching(cwd, exts=['.xsl', '.jpg', '.gif', '.png']):
+    for (parent, dirs, files) in os.walk(cwd):
+        for f in files:
+            if '.git' not in parent and '.git' not in f and \
+               not any(f.endswith(ext) for ext in exts):
+                os.remove(fmt_dir(parent, f))
